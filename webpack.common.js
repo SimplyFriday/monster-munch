1f30f660dabc9faf7852dcf9278cde6d4eac5059
Josh Greenlaw <jgreenlaw91@gmail.com>
2021-07-16 23:22:12 -0400

    Title change, quick update for some template packages

--- webpack.common.js
+++ webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
-      title: "Excalibur Webpack Sample",
+      title: "Callum Horror Jam (change me)",
     }),
   ],
 };
