4865ac3ee04889ab4c45fbdc1f3bf1dcf40b17dd
Josh Greenlaw <jgreenlaw91@gmail.com>
2021-07-23 22:01:48 -0400

    Ui resizing works, neat!

--- src/actors/objects/uiHelper.ts
+++ src/actors/objects/uiHelper.ts
@@ -1,4 +1,4 @@
-import { Scene, ScreenElement, Texture, vec, Sprite } from "excalibur";
+import { Scene, ScreenElement, Texture, vec, Sprite, Timer } from "excalibur";
 import { Resources } from "../../resources";
 import { LevelBase } from "../../scenes/levels/levelBase";
 import { BalloonIconSprites } from "./balloonIconSprites";
@@ -9,8 +9,8 @@ export abstract class UIHelper {
     private static iconWidth:number = 50;
     private static iconHeight:number = 50;
 
-    private static createUIIcon(texture: Texture|Sprite, xPos: number, yPos: number) {
-        let se = new ScreenElement({
+    private static createUIIcon(texture: Texture|Sprite, xPos: number, yPos: number): ViewportLockedUIElement {
+        let se = new ViewportLockedUIElement({
             width:this.iconWidth,
             height: this.iconHeight,
             pos: vec(xPos,yPos)
@@ -35,8 +35,8 @@ export abstract class UIHelper {
         return s;
     }
 
-    private static createUIToggleButton (spriteOn:Sprite, spriteOff:Sprite, xPos:number, yPos:number): ScreenElement {
-        let se = new ScreenElement({
+    private static createUIToggleButton (spriteOn:Sprite, spriteOff:Sprite, xPos:number, yPos:number): ViewportLockedUIElement {
+        let se = new ViewportLockedUIElement({
             width:this.iconWidth,
             height: this.iconHeight,
             pos: vec(xPos,yPos)
@@ -49,7 +49,10 @@ export abstract class UIHelper {
     }
 
     public static addLevelUI (scene:LevelBase) {
-        let musicToggle = this.createUIToggleButton(BalloonIconSprites.MusicBalloon, BalloonIconSprites.MusicBalloonOff, 800, 50);
+        let musicToggle = this.createUIToggleButton(BalloonIconSprites.MusicBalloon, BalloonIconSprites.MusicBalloonOff, window.innerWidth - 220, 50);
+        musicToggle.xRelativeTo = "right";
+        musicToggle.x = -220;
+        musicToggle.y = 50;
         
         musicToggle.on ('pointerup', (event) =>{
             scene.toggleMusic();
@@ -63,13 +66,85 @@ export abstract class UIHelper {
 
         scene.add(musicToggle);
 
-        let hp1 = this.createUIIcon(ItemIconSprites.Heart, 900, 50);
-        let hp2 = this.createUIIcon(ItemIconSprites.Heart, 930, 50);
-        let hp3 = this.createUIIcon(ItemIconSprites.Heart, 960, 50);
-        
+        let hp1 = this.createUIIcon(ItemIconSprites.Heart, window.innerWidth - 120, 50);
+        hp1.xRelativeTo = "right";
+        hp1.x = -120;
+        hp1.y = 50;
+
+        let hp2 = this.createUIIcon(ItemIconSprites.Heart, window.innerWidth - 90, 50);
+        hp2.xRelativeTo = "right";
+        hp2.x = -90;
+        hp2.y = 50;
+
+        let hp3 = this.createUIIcon(ItemIconSprites.Heart, window.innerWidth - 60, 50);
+        hp3.xRelativeTo = "right";
+        hp3.x = -60;
+        hp3.y = 50;
+
         scene.add (hp1);
         scene.add (hp2);
         scene.add (hp3);
 
+        let timer = new UITimer(50);
+        timer.uiElements.push(musicToggle);
+        timer.uiElements.push(hp1);
+        timer.uiElements.push(hp2);
+        timer.uiElements.push(hp3);
+        
+        scene.add(timer);
+        timer.reset();
+    }
+}
+
+export class ViewportLockedUIElement extends ScreenElement {
+    public x:number;
+    public y:number;
+    public xRelativeTo:string;
+    public yRelativeTo:string;
+}
+export class UITimer extends Timer {
+    public uiElements:ViewportLockedUIElement[] = [];
+
+    constructor (interval:number) {
+        super({
+            interval:interval,
+            repeats: true
+        })
+
+        console.log("added ui timer");
+        this.on(this.updateUILocations);
+    }
+    private updateUILocations () {
+        this.uiElements.forEach(element => {
+            let xPos:number, yPos:number;
+
+            if (element.xRelativeTo) {
+                switch(element.xRelativeTo) {
+                    case "right":
+                        xPos = window.innerWidth + element.x;
+                        break;
+                    case "left":
+                        xPos = element.x;
+                        break;
+                }
+            } else {
+                xPos = element.x;
+            }
+
+            if (element.yRelativeTo) {
+                switch(element.yRelativeTo) {
+                    case "bottom":
+                        xPos = window.innerHeight + element.y;
+                        break;
+                    case "top":
+                        xPos = element.y;
+                        break;
+                }
+            } else {
+                yPos = element.y
+            }
+
+            element.pos = vec(xPos, yPos);
+        });
     }
 }
